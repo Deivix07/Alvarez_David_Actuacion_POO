8a12b2da130bad979b0f04e90f595d84683e8a04
Deivix07 <davidalvarezdm1226@gmail.com>
2025-02-18 23:40:16 -0500

    first commit

---
+++ Actuacion_POO/Operacion.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actuacion_POO
+{
+    internal interface Operacion
+    {
+        int sumar();
+        int restar();
+        int multiplicar();
+        int dividir();
+    }
+}
